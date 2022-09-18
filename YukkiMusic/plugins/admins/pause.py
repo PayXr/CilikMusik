76abfa0473cd28683ab61393c3657afcf7908e97
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-18 08:28:45 -0700

    5

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_off
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from CilikMusic import app
+from CilikMusic.core.call import Yukki
+from CilikMusic.utils.database import is_music_playing, music_off
+from CilikMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
