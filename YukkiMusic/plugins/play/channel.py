def50d4c072a823e0d9f943047c6ce922fbba7b6
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-18 08:35:50 -0700

    Update channel.py

--- YukkiMusic/plugins/play/channel.py
+++ YukkiMusic/plugins/play/channel.py
@@ -12,9 +12,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database import set_cmode
-from YukkiMusic.utils.decorators.admins import AdminActual
+from CilikMusic import app
+from CilikMusic.utils.database import set_cmode
+from CilikMusic.utils.decorators.admins import AdminActual
 
 ### Multi-Lang Commands
 CHANNELPLAY_COMMAND = get_command("CHANNELPLAY_COMMAND")
