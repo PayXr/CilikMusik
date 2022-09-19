4373ec54c3238ffbf2b465b41ea6c42f9e658ad2
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 04:17:19 -0700

    Update language.py

--- YukkiMusic/utils/decorators/language.py
+++ YukkiMusic/utils/decorators/language.py
@@ -9,8 +9,8 @@
 
 
 from strings import get_string
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_lang, is_commanddelete_on,
+from CilikMusic.misc import SUDOERS
+from CilikMusic.utils.database import (get_lang, is_commanddelete_on,
                                        is_maintenance)
 
 
