4b50e0f33061d93ef3c2cc89c3b2689a6b622cda
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 03:58:17 -0700

    Update videomode.py

--- YukkiMusic/plugins/sudo/videomode.py
+++ YukkiMusic/plugins/sudo/videomode.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from CilikMusic import app
+from CilikMusic.misc import SUDOERS
+from CilikMusic.utils.database import add_off, add_on
+from CilikMusic.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
