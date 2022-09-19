31d78130b7b47eaa865fc30d623b052eb1e1d96c
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 07:34:22 -0700

    Update and rename YukkiMusic/plugins/play/playlist.py to CilikMusic/plugins/play/playlist.py

--- CilikMusic/plugins/play/playlist.py
+++ CilikMusic/plugins/play/playlist.py
@@ -25,7 +25,7 @@ from CilikMusic.utils.decorators.language import language, languageCB
 from CilikMusic.utils.inline.playlist import (botplaylist_markup,
                                               get_playlist_markup,
                                               warning_markup)
-from CilikMusic.utils.pastebin import Yukkibin
+from CilikMusic.utils.pastebin import Cilikbin
 from CilikMusic.utils.stream.stream import stream
 
 # Command
@@ -55,7 +55,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await Yukkibin(msg)
+    link = await Cilikbin(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
