f2d7b0c4cf0d633aa946780605be650c91cbe89b
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 04:36:10 -0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -58,6 +58,6 @@ for filename in os.listdir(r"./strings/langs/"):
         ]
     except:
         print(
-            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
+            "Ada beberapa masalah dengan file bahasa di dalam bot. Silakan laporkan ke proyek Cilik di @CilikSupport di Telegram"
         )
         sys.exit()
