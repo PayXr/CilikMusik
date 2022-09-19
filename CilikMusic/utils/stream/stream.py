9c109423471622b162a716a2558d67b77dc9955a
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 08:01:49 -0700

    Update and rename YukkiMusic/utils/stream/stream.py to CilikMusic/utils/stream/stream.py

--- CilikMusic/utils/stream/stream.py
+++ CilikMusic/utils/stream/stream.py
@@ -15,7 +15,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from CilikMusic import Carbon, YouTube, app
-from CilikMusic.core.call import Yukki
+from CilikMusic.core.call import Cilik
 from CilikMusic.misc import db
 from CilikMusic.utils.database import (add_active_chat,
                                        add_active_video_chat,
@@ -25,7 +25,7 @@ from CilikMusic.utils.exceptions import AssistantErr
 from CilikMusic.utils.inline.play import (stream_markup,
                                           telegram_markup)
 from CilikMusic.utils.inline.playlist import close_markup
-from CilikMusic.utils.pastebin import Yukkibin
+from CilikMusic.utils.pastebin import Cilikbin
 from CilikMusic.utils.stream.queue import put_queue, put_queue_index
 from CilikMusic.utils.thumbnails import gen_thumb
 
@@ -49,7 +49,7 @@ async def stream(
         if not await is_video_allowed(chat_id):
             raise AssistantErr(_["play_7"])
     if forceplay:
-        await Yukki.force_stop_stream(chat_id)
+        await Cilik.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['playlist_16']}\n\n"
         count = 0
@@ -98,7 +98,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_16"])
-                await Yukki.join_call(
+                await Cilik.join_call(
                     chat_id, original_chat_id, file_path, video=status
                 )
                 await put_queue(
@@ -129,7 +129,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await Yukkibin(msg)
+            link = await Cilikbin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -179,7 +179,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Cilik.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -233,7 +233,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Cilik.join_call(
                 chat_id, original_chat_id, file_path, video=None
             )
             await put_queue(
@@ -287,7 +287,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Cilik.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -348,7 +348,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Yukki.join_call(
+            await Cilik.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -400,7 +400,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Yukki.join_call(
+            await Cilik.join_call(
                 chat_id,
                 original_chat_id,
                 link,
