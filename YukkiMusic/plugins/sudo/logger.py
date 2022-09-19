a93f07022e61e86c2e6b4b1b226ac673a86f06d0
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 03:56:54 -0700

    Update logger.py

--- YukkiMusic/plugins/sudo/logger.py
+++ YukkiMusic/plugins/sudo/logger.py
@@ -11,10 +11,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from CilikMusic import app
+from CilikMusic.misc import SUDOERS
+from CilikMusic.utils.database import add_off, add_on
+from CilikMusic.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
