bcfc973c5ec7bfd23d02427b2aa31c4988a87db8
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 07:44:23 -0700

    Update ping.py

--- YukkiMusic/plugins/tools/ping.py
+++ YukkiMusic/plugins/tools/ping.py
@@ -15,7 +15,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
 from CilikMusic import app
-from CilikMusic.core.call import Yukki
+from CilikMusic.core.call import Cilik
 from CilikMusic.utils import bot_sys_stats
 from CilikMusic.utils.decorators.language import language
 
@@ -36,7 +36,7 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"],
     )
     start = datetime.now()
-    pytgping = await Yukki.ping()
+    pytgping = await Cilik.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
