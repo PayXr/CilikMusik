84863022937167d19e40849c0f1dba6dc083c3f0
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 07:05:05 -0700

    Update seek.py

--- YukkiMusic/plugins/admins/seek.py
+++ YukkiMusic/plugins/admins/seek.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from CilikMusic import YouTube, app
-from CilikMusic.core.call import Yukki
+from CilikMusic.core.call import Cilik
 from CilikMusic.misc import db
 from CilikMusic.utils import AdminRightsCheck, seconds_to_min
 
@@ -70,7 +70,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
         if n == 0:
             return await message.reply_text(_["admin_30"])
     try:
-        await Yukki.seek_stream(
+        await Cilik.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
