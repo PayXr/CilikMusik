1f425fb3d95ad2eb0f5f4dc5282aab7d5addfd94
PayXr <103725749+PayXr@users.noreply.github.com>
2022-09-19 03:57:29 -0700

    Update sudoers.py

--- YukkiMusic/plugins/sudo/sudoers.py
+++ YukkiMusic/plugins/sudo/sudoers.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_sudo, remove_sudo
-from YukkiMusic.utils.decorators.language import language
+from CilikMusic import app
+from CilikMusic.misc import SUDOERS
+from CilikMusic.utils.database import add_sudo, remove_sudo
+from CilikMusic.utils.decorators.language import language
 
 # Command
 ADDSUDO_COMMAND = get_command("ADDSUDO_COMMAND")
